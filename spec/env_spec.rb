5ec970ff16b6483cb994a5da8fcda197a3635d63
Postmodern <postmodern.mod3@gmail.com>
2020-12-26 23:39:20 -0800

    Used transpec to upgrade to RSpec 3.x.

--- spec/env_spec.rb
+++ spec/env_spec.rb
@@ -6,32 +6,32 @@ describe Env do
   let(:var_value) { ENV[var_name] }
 
   it "should have a VERSION constant" do
-    subject.const_get('VERSION').should_not be_empty
+    expect(subject.const_get('VERSION')).not_to be_empty
   end
 
   it "should provide direct Hash access to env variables" do
-    subject[var_name].should == var_value
+    expect(subject[var_name]).to eq(var_value)
   end
 
   it "should allow directly setting env variables" do
     subject['FOO'] = 'bar'
 
-    subject['FOO'].should == 'bar'
+    expect(subject['FOO']).to eq('bar')
   end
 
   it "should provide transparent CONSTANT access to env variables" do
-    subject.const_get(var_name).should == var_value
+    expect(subject.const_get(var_name)).to eq(var_value)
   end
 
   it "should provide transparent method access to env variables" do
     method_name = var_name.downcase
 
-    subject.send(method_name).should == var_value
+    expect(subject.send(method_name)).to eq(var_value)
   end
 
   it "should allow setting of environment variables via methods" do
     subject.shell = '/bin/zsh'
 
-    subject['SHELL'].should == '/bin/zsh'
+    expect(subject['SHELL']).to eq('/bin/zsh')
   end
 end
--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -9,7 +9,7 @@ describe Env::Methods do
   let(:shell) { '/bin/bash' }
 
   before(:each) do
-    subject.stub!(:env).and_return(
+    allow(subject).to receive(:env).and_return(
       'PATH' => '/usr/local/bin:/usr/bin:/bin',
       'HOME' => home,
       'TERM' => term,
@@ -22,83 +22,83 @@ describe Env::Methods do
   end
 
   it "should parse the contents of the PATH variable" do
-    subject.paths.should == [
+    expect(subject.paths).to eq([
       Pathname.new('/usr/local/bin'),
       Pathname.new('/usr/bin'),
       Pathname.new('/bin')
-    ]
+    ])
   end
 
   it "should provide access to the HOME variable" do
-    subject.home.should == Pathname.new(home)
+    expect(subject.home).to eq(Pathname.new(home))
   end
 
   it "should use the USERPROFILE variable if HOME is not set" do
-    subject.stub!(:env).and_return('USERPROFILE' => home)
+    allow(subject).to receive(:env).and_return('USERPROFILE' => home)
 
-    subject.home.should == Pathname.new(home)
+    expect(subject.home).to eq(Pathname.new(home))
   end
 
   it "should use HOMEDRIVE and HOMEPATH if HOME is not set" do
     drive = 'C:'
 
-    subject.stub!(:env).and_return(
+    allow(subject).to receive(:env).and_return(
       'HOMEDRIVE' => drive,
       'HOMEPATH' => home
     )
 
-    subject.home.should == Pathname.new(drive + home)
+    expect(subject.home).to eq(Pathname.new(drive + home))
   end
 
   it "should attempt to expand '~' if none of the HOME variables are set" do
-    subject.stub!(:env).and_return({})
+    allow(subject).to receive(:env).and_return({})
 
-    subject.home.should be_directory
+    expect(subject.home).to be_directory
   end
 
   it "should parse the LANG variable" do
     name, encoding = subject.lang
 
-    name.should == 'en_US'
-    encoding.should == 'UTF8'
+    expect(name).to eq('en_US')
+    expect(encoding).to eq('UTF8')
   end
 
   it "should return an empty Array if LANG is not set" do
-    subject.stub!(:env).and_return({})
+    allow(subject).to receive(:env).and_return({})
 
-    subject.lang.should be_empty
+    expect(subject.lang).to be_empty
   end
 
   it "should parse the COLUMNS variable" do
-    subject.columns.should == 80
+    expect(subject.columns).to eq(80)
   end
 
   it "should parse the LINES variable" do
-    subject.lines.should == 10
+    expect(subject.lines).to eq(10)
   end
 
   it "should provide access to the SHELL variable" do
-    subject.shell.should == shell
+    expect(subject.shell).to eq(shell)
   end
 
   it "should determine the program name of the current Shell" do
-    subject.shell_name.should == 'bash'
+    expect(subject.shell_name).to eq('bash')
   end
 
   it "should determine the current Terminal" do
-    subject.terminal.should == term
+    expect(subject.terminal).to eq(term)
   end
 
   it "should check COLORTERM before the TERM variable" do
-    subject.stub!(:env).and_return(
+    allow(subject).to receive(:env).and_return(
       'COLORTERM' => 'gnome-terminal',
       'TERM' => term
     )
 
-    subject.terminal.should == 'gnome-terminal'
+    expect(subject.terminal).to eq('gnome-terminal')
   end
 
   it "should check if DEBUG was set" do
-    subject.should be_debug
+    expect(subject).to be_debug
   end
 end
